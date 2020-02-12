e80de2a9dfc06f820faa53fd255fc8f37d8d038b
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-02-12 15:31:32 +0200

    fix slug

--- slug.js
+++ slug.js
@@ -2,7 +2,7 @@ module.exports = text => {
     if(text == +text) {
         return text = `Пост номер ${text}`
     } else {
-    text = text.replace(/[0-9#]/g, '').replace(/[^A-Z0-9]/gi, '').replace(/[-_]/g, ' ');
+    text = text.replace(/[0-9#]/g, '').replace(/[-_]/g, ' ');
     const rus = ["щ", "ш", "ч", "ц", "ю", "я", "ё", "ж", "ъ", "ы", "э", "а", "б", "в", "г", "д", "е", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ь"];
     const eng = ["shh", "sh", "ch", "cz", "yu", "ya", "yo", "zh", "``", "y'", "e`", "a", "b", "v", "g", "d", "e", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "x", "`"];
       for(let x = 0; x < rus.length; x++) {
